d26401176e9fe9d3bcc410b09851d091f478851b
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-06 02:39:47 +0000

    automatically backed up by learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -5,7 +5,7 @@ export default function manageTodo(state = {
     case 'ADD_TODO':
       return { todos: state.todos.concat(action.todo.text)};
 
-  default:
-    return state;
+    default:
+      return state;
   }
 }
